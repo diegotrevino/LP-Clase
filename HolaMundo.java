6c3757339e5752492edf9eb505f034aee497ba18
Juan Diego <diegobfcy@gmail.com>
2023-08-21 10:01:59 -0500

    Agregado metodo para acumular vidas

---
+++ HolaMundo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		int vida,rp;
+		boolean fl = false;
+		vida = 0;
+		Scanner sc = new Scanner(System.in);
+		do {
+			vida = numeroVidas(vida);
+			System.out.println("Vida: "+vida+"\t¿Cual es el numero secreto?");
+			rp = sc.nextInt();
+			if(rp == 1234)
+				fl=true;
+		}while(vida < 3 && !fl);
+		if(fl == true)
+			System.out.println("Adiviniaste!!! ");
+		else
+			System.out.println("ups, perdiste... ");
+	}
+	public static int numeroVidas(int vida) {
+		vida++;
+		return vida;
+	}
+}
